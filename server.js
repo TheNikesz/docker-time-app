0b376cac1aeff2979b58b355f8e8e79db5a252ae
TheNikesz <64257234+TheNikesz@users.noreply.github.com>
2021-11-07 18:51:21 +0100

    Create server.js

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
-'use strict';
-
-const express = require('express');
+import express from 'express';
+import fetch from'node-fetch';
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const DATE = new Date().toLocaleString('pl-PL');
+const AUTHOR = 'Blazej Typek';
+
+// Functions
+async function getTimeFromIp(ip) {
+  try {
+    var response = await fetch(`http://ip-api.com/json/${ip}`);
+    var data = await response.json();
+    
+    var timezone = data['timezone'];
+    if (timezone == undefined) {
+      timezone = "Europe/Warsaw";
+    }
+    var localDate = new Date().toLocaleString('pl-PL', {timezone});
+
+    return `${localDate}`;
+  } catch (e) {
+    console.log(e);
+    return 'Pobieranie danych z API nie powiodlo sie.';
+  }
+}
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+console.log(`Data, czas uruchomienia: ${DATE}`);
+console.log(`Autor: ${AUTHOR}`);
+console.log(`Port: ${PORT}`);
+
+app.set('trust proxy', true)
+app.use(async (req, res) => {
+  var localDate = await getTimeFromIp(req.ip);
+  res.send(`<p>IP: ${req.ip}</p><p>Data, czas:  ${localDate}</p>`);
+  
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, '0.0.0.0');
